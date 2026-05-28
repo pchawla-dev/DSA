156c346908e887e2b45f4f54d538b6ef7fc659ec
pchawla-dev <poonamchawla.work@gmail.com>
2026-05-28 20:20:39 +0530

    Unique Element

---
+++ src/leetcode/LC136.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class LC136 {
+	 public int singleNumber(int[] nums) {
+	        int n = nums.length;
+	        int ans = nums[0];
+	        for(int i=1; i<n; i++) {
+	            ans = ans ^ nums[i];
+
+	        }
+	        return ans;
+	  }
+}
